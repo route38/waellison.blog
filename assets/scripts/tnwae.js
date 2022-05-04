3b9fbe0be8c9ac1bfb218e3b7f5779eba0b924fa
William Ellison <waellison@gmail.com>
2022-05-03 21:42:23 -0400

    Removed unneeded assets.
    These have been pulled up to <cdn.waellison.cf>.

--- assets/scripts/tnwae.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-        let here = window.location.pathname;
-
-        $("nav menu li a[href='" + here + "']").each(function (idx, elem) {
-                $(elem).addClass("active");
-        });
-});
-
