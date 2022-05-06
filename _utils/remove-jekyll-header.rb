4769bd8cfe8e632a798096f6186e9497f3dafd27
William Ellison <97247746+waellison@users.noreply.github.com>
2022-05-06 15:46:00 -0400

    removal: removed unused utilities

--- _utils/remove-jekyll-header.rb
+++
@@ -1,9 +0,0 @@
-# Get a word count across the whole blog.
-ARGV.each do |file|
-  re = Regexp.try_convert(/(---.*---)(.*)/m)
-  parts = re.match(File.read(file))
-  if not parts.nil? then
-    puts parts[2]
-  end
-end
-
